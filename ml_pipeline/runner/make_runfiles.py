6e9e3d99e01c810d821c86604cacc817d3bb4247
Ario Sadafi <ario.sadafi@gmail.com>
2023-07-26 23:51:34 +0200

    fixing

--- ml_pipeline/runner/make_runfiles.py
+++ ml_pipeline/runner/make_runfiles.py
@@ -2,6 +2,7 @@ import os
 
 experiments = ["0", "1", "2", "3", "4"]
 folds = [0,1,2,3,4]
+kind = ["sex", "age"]
 
 if os.path.exists("run.sh"):
     os.remove("run.sh")
@@ -13,24 +14,29 @@ sh.writelines("#!/bin/bash\n\n")
 
 lines = f.readlines()
 
-for exp in experiments:
-    for fld in folds:
-        if os.path.exists("sb-" + exp + "-" + str(fld) + ".cmd"):
-            os.remove("sb-" + exp + "-" + str(fld) + ".cmd")
-for exp in experiments:
-    for fld in folds:
-            c = lines.copy()
-            for i in range(len(c)):
-                c[i] = c[i].replace("@fld@", str(fld))
-                c[i] = c[i].replace("@exp@", str(exp))
+for k in kind:
+    for exp in experiments:
+        for fld in folds:
+            if os.path.exists("sb-" + k + "-" + exp + "-" + str(fld) + ".cmd"):
+                os.remove("sb-" + k + "-" + exp + "-" + str(fld) + ".cmd")
 
-            with open("sb-" + exp + "-" + str(fld) + ".cmd", "w") as g:
-                g.writelines(c)
-                g.flush()
-                g.close()
 
-            sh.writelines("sbatch " + "sb-" + exp + "-" + str(fld) + ".cmd\n")
-        # sh.writelines("sleep 0.5\n")
+for k in kind:
+    for exp in experiments:
+        for fld in folds:
+                c = lines.copy()
+                for i in range(len(c)):
+                    c[i] = c[i].replace("@fld@", str(fld))
+                    c[i] = c[i].replace("@exp@", str(exp))
+                    c[i] = c[i].replace("@kind@", str(k))
+
+                with open("sb-" + k + "-" + exp + "-" + str(fld) + ".cmd", "w") as g:
+                    g.writelines(c)
+                    g.flush()
+                    g.close()
+
+                sh.writelines("sb-" + k + "-" + exp + "-" + str(fld) + ".cmd\n")
+            # sh.writelines("sleep 0.5\n")
 
 sh.flush()
 sh.close()
