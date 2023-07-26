d37d522abf6d9956c10ea22c38716d992bcad589
Ario Sadafi <ario.sadafi@gpusrv68.scidom.de>
2023-07-26 17:10:30 +0200

    runner

---
+++ ml_pipeline/runner/make_runfiles.py
@@ -0,0 +1,60 @@
+import os
+
+experiments = ["1", "2", "3", "4"]
+#mths = ["lb", "ewc", "icarl", "ucicarl"]
+mths = ["mas"]
+
+if os.path.exists("run.sh"):
+    os.remove("run.sh")
+
+f = open("run.template", "r")
+sh = open("run.sh", "w")
+
+sh.writelines("#!/bin/bash\n\n")
+
+lines = f.readlines()
+
+for exp in experiments:
+    for mth in mths:
+        if os.path.exists("sb-" + exp + "-" + mth + ".cmd"):
+            os.remove("sb-" + exp + "-" + mth + ".cmd")
+for exp in experiments:
+    for mth in mths:
+        
+        if exp == "4":
+            for ds in ["aml", "pbc", "mll"]:
+                c = lines.copy()
+                for i in range(len(c)):
+                    c[i] = c[i].replace("@mth@", str(mth))
+                    c[i] = c[i].replace("@exp@", str(exp))
+                    c[i] = c[i].replace("@ds@", ds)
+
+
+                with open("sb-" + exp + "-" + mth + "-" + ds +".cmd", "w") as g:
+                    g.writelines(c)
+                    g.flush()
+                    g.close()
+                
+                sh.writelines("sbatch " + "sb-" + exp + "-" + mth + "-" + ds +".cmd\n")
+
+        
+        else: 
+            c = lines.copy()
+            for i in range(len(c)):
+                c[i] = c[i].replace("@mth@", str(mth))
+                c[i] = c[i].replace("@exp@", str(exp))
+                c[i] = c[i].replace("@ds@", "")
+
+
+            with open("sb-" + exp + "-" + mth + ".cmd", "w") as g:
+                g.writelines(c)
+                g.flush()
+                g.close()
+
+            sh.writelines("sbatch " + "sb-" + exp + "-" + mth + ".cmd\n")
+        # sh.writelines("sleep 0.5\n")
+
+sh.flush()
+sh.close()
+
+
